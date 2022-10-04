5318fc49ee851988e363331f845adc424c6e8026
RohithYanapu <110309836+RohithYanapu@users.noreply.github.com>
2022-10-03 22:36:02 -0500

    Added functionality requirement 1

--- src/Calculator/MainPage.xaml.cs
+++ src/Calculator/MainPage.xaml.cs
@@ -1,8 +1,10 @@
-﻿namespace Calculator;
+﻿using System.Data;
+
+namespace Calculator;
 
 public partial class MainPage : ContentPage
 {
-    
+
     public MainPage()
     {
         InitializeComponent();
@@ -15,15 +17,16 @@ public partial class MainPage : ContentPage
     string mathOperator;
     double firstNumber, secondNumber;
     string decimalFormat = "N0";
-    
-    
+    string equation = "";
+    string displayText = "";
 
     void OnSelectNumber(object sender, EventArgs e)
     {
-    
+
         Button button = (Button)sender;
         string pressed = button.Text;
-
+        this.equation += pressed;
+        this.displayText += pressed;
         currentEntry += pressed;
 
         if ((this.resultText.Text == "0" && pressed == "0")
@@ -46,11 +49,13 @@ public partial class MainPage : ContentPage
     void OnSelectOperator(object sender, EventArgs e)
     {
         LockNumberValue(resultText.Text);
-
+        
         currentState = -2;
         Button button = (Button)sender;
         string pressed = button.Text;
-        mathOperator = pressed;            
+        this.equation += pressed;
+        this.displayText += pressed;
+        mathOperator = pressed;
     }
 
     private void LockNumberValue(string text)
@@ -78,34 +83,46 @@ public partial class MainPage : ContentPage
         currentState = 1;
         decimalFormat = "N0";
         this.resultText.Text = "0";
-        currentEntry = string.Empty;
+        currentEntry = "";
+        this.CurrentCalculation.Text = "";
+        this.equation = "";
+        this.displayText = "";
     }
 
     void OnCalculate(object sender, EventArgs e)
     {
+        if (mathOperator != "Sqrt")
+        {
+            DataTable dt = new DataTable();
+            System.Diagnostics.Debug.WriteLine(this.equation);
+            var v = dt.Compute(this.equation, "");
+            this.resultText.Text = v.ToString();
+        }
         if (currentState == 2)
         {
-            if(secondNumber == 0)
+            if (secondNumber == 0)
                 LockNumberValue(resultText.Text);
 
             double result = Calculator.Calculate(firstNumber, secondNumber, mathOperator);
 
-            this.CurrentCalculation.Text = $"{firstNumber} {mathOperator} {secondNumber}";
+            this.CurrentCalculation.Text = this.displayText;
 
-            this.resultText.Text = result.ToTrimmedString(decimalFormat);
+            
             firstNumber = result;
+
             secondNumber = 0;
             currentState = -1;
             currentEntry = string.Empty;
+            mathOperator = "";
         }
-    }    
+    }
 
     void OnNegative(object sender, EventArgs e)
     {
         if (currentState == 1)
         {
             secondNumber = -1;
-            mathOperator = "×";
+            mathOperator = "*";
             currentState = 2;
             OnCalculate(this, null);
         }
@@ -113,14 +130,13 @@ public partial class MainPage : ContentPage
 
     void OnPercentage(object sender, EventArgs e)
     {
-        if (currentState == 1)
-        {
-            LockNumberValue(resultText.Text);
-            decimalFormat = "N2";
-            secondNumber = 0.01;
-            mathOperator = "×";
-            currentState = 2;
-            OnCalculate(this, null);
-        }
+        LockNumberValue(resultText.Text);
+        this.equation += "*0.01";
+        this.displayText += "%";
+        decimalFormat = "N2";
+        secondNumber = 0.01;
+        mathOperator = "*";
+        currentState = 2;
+
     }
 }
